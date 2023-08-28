20c522a80433571bfbdf659a39165f41469bb325
gowthami <gowthami@LAPTOP-54CGIQF6>
2023-08-28 01:18:28 -0700

    rrrr

--- src/test/java/actions/doubleclick.java
+++ src/test/java/actions/doubleclick.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+//this is second workspace
 public class doubleclick {
 
 	public static void main(String[] args) {
--- src/test/java/vtigerFinalScripts/CreateEventTest.java
+++ src/test/java/vtigerFinalScripts/CreateEventTest.java
@@ -8,20 +8,20 @@ import org.testng.asserts.SoftAssert;
 import genericliraries.BaseClass;
 import genericliraries.IConstantPath;
 
-public class CreateEventTest extends BaseClass{
+public  class CreateEventTest extends BaseClass{
 	@Test
 	public void createEventTest()
 	{
 		SoftAssert soft=new SoftAssert();
 		Map<String,String> map=excel.readFromExcel("EventsTestData", "CreateNewEvent");
-		home.selectFromQuickCreate(web, map.get("Quick Create"));
+		home.selectFromQuickCreate(web, map.get("QuickCreate"));
 		soft.assertEquals(createEvent.getPageHeader(), "Create To Do");
 		String subject=map.get("subject")+jutil.generateRandomNum(100);
 		createEvent.setSubject(subject);
 		createEvent.clickOnStartDate();
-		createEvent.chooseRequiredDate(web, map.get("Start date"), jutil);
+		createEvent.chooseRequiredDate(web, map.get("StartDate"), jutil);
 		createEvent.clickOnDueDate();
-		createEvent.chooseRequiredDate(web, map.get("Due Date"), jutil);
+		createEvent.chooseRequiredDate(web, map.get("Duedate"), jutil);
 		createEvent.clickSaveButton();
 		soft.assertTrue(newEventInfo.getPageHeader().contains(subject));
 		if(newEventInfo.getPageHeader().contains(subject))
